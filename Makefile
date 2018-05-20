a84c090f016a05157aca70cfb4fbfaf11ad28fc6
Dantran93 <daniel.tranfaglia@yahoo.com>
2018-05-20 14:40:02 -0700

    Setting the stage for Stacks and StackItems, Makefile is updated

--- Makefile
+++ Makefile
@@ -1,16 +1,19 @@
 # RandomMazeGenerator Makefile
 # Author: Daniel Tranfaglia
 
-all: Location2D.o RandomMazeGenerator.o
-	gcc -Wall Location2D.o RandomMazeGenerator.o -o RandomMazeGenerator
+all: Location2D.o RandomMazeGenerator.o Stack.o
+	gcc -Wall Stack.o Location2D.o RandomMazeGenerator.o -o RandomMazeGenerator
 
 
 
-RandomMazeGenerator.o:
+RandomMazeGenerator.o: RandomMazeGenerator.c
 	gcc -Wall -c RandomMazeGenerator.c -o RandomMazeGenerator.o
 
 Location2D.o: Location2D.c
 	gcc -Wall -c Location2D.c -o Location2D.o
+
+Stack.o: Stack.c
+	gcc -Wall -c Stack.c -o Stack.o
 
 
 
---
+++ Stack.h
@@ -0,0 +1,47 @@
+/******************************************************************************
+ * Stack.h
+ * Author: Daniel Tranfaglia
+ *
+ * Purpose: Contains the structs, constants, and function prototypes associated
+ *      with stacks and the items contained within them.
+ *****************************************************************************/
+
+
+
+#ifndef STACK_H
+#define STACK_H
+
+
+
+// Struct to define an item in a stack
+typedef struct StackItem
+{
+    void *item;               // Generic item
+    struct StackItem *next;   // Next item
+} StackItem;
+
+
+
+// Struct to define a stack of items
+typedef struct Stack
+{
+    StackItem *top;      // Top item in the stack
+} Stack;
+
+
+
+// Constants
+#define ERROR_OCCURRED 1
+
+
+
+// Function Prototypes
+Stack     * new_Stack     ();
+StackItem * new_StackItem (void *newItem);
+int         push          (Stack *stack, StackItem *newItem);
+int         pop           (Stack *stack);
+void        peek          (Stack *stack);
+
+
+
+#endif
